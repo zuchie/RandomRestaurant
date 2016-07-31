dc08eaad63428d6584f15990ccb347f3b84ccfab
zuchie <zuchie@gmail.com>
2016-07-31 15:31:53 -0700

    1st commit, can get response from fixed business search.

--- randomRestaurant/ViewController.swift
+++ randomRestaurant/ViewController.swift
@@ -10,6 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var token: UITextField!
+    
+    @IBAction func start() {
+        let myUrl = "https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972"
+        
+        let urlObj = NSURL(string: myUrl)
+        
+        let request = NSMutableURLRequest(URL: urlObj!)
+        
+        request.HTTPMethod = "GET"
+        
+        let access_token = token.text!
+        
+        request.addValue("Bearer \(access_token)", forHTTPHeaderField: "Authorization")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            // Check for error
+            if error != nil
+            {
+                print("error=\(error)")
+                return
+            }
+            
+            // Print out response string
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("responseString = \(responseString)")
+            
+            // Convert server json response to NSDictionary
+            do {
+                if let convertedJsonIntoDict = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
+                    
+                    // Print out dictionary
+                    print(convertedJsonIntoDict)
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
