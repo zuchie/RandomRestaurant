b98435cab0bae266390034f1798a9c791cc14dbf
zuchie <zuchie@gmail.com>
2016-08-01 22:57:08 -0700

    Separate Control from View for making URL request

---
+++ RestaurantBrain.swift
@@ -0,0 +1,113 @@
+//
+//  RestaurantBrain.swift
+//  randomRestaurant
+//
+//  Created by Zhe Cui on 8/1/16.
+//  Copyright © 2016 Zhe Cui. All rights reserved.
+//
+
+import Foundation
+
+class RestaurantBrain {
+    
+    private struct urlParameters {
+        var term: String?
+        var latitude: Double?
+        var longitude: Double?
+        var limit: Int?
+    }
+
+    private var urlParams = urlParameters()
+    
+    func getUrlParameters(term: String?, latitude: Double?, longitude: Double?, limit: Int?) {
+        //urlParams = urlParameters(term: term!, latitude: latitude!, longitude: longitude!, limit: limit!)
+        //urlParams = urlParameters()
+
+        if term != nil {
+            urlParams.term = term
+        }
+        if latitude != nil {
+            urlParams.latitude = latitude
+        }
+        if longitude != nil {
+            urlParams.longitude = longitude
+        }
+        if limit != nil {
+            urlParams.limit = limit
+        }
+    }
+    
+    private var bizSearchUrl = ""
+    
+    func makeBizSearchUrl(baseUrl: String) {
+        bizSearchUrl = baseUrl
+        
+        if urlParams.term != nil {
+            bizSearchUrl += "term=\(urlParams.term!)"
+        }
+        if urlParams.latitude != nil {
+            bizSearchUrl += "&latitude=\(urlParams.latitude!)"
+        }
+        if urlParams.longitude != nil {
+            bizSearchUrl += "&longitude=\(urlParams.longitude!)"
+        }
+        if urlParams.limit != nil {
+            bizSearchUrl += "&limit=\(urlParams.limit!)"
+        }
+    }
+    
+    func makeUrlRequest(token: String) {
+        
+        let urlObj = NSURL(string: bizSearchUrl)
+        let request = NSMutableURLRequest(URL: urlObj!)
+        request.HTTPMethod = "GET"
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            // Check for error
+            if error != nil
+            {
+                print("error=\(error)")
+                return
+            }
+            
+            // Print out response string
+            //let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            //print("responseString = \(responseString)")
+            
+            // Convert server json response to NSDictionary
+            do {
+                if let convertedJsonIntoDict = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
+                    
+                    // Print out dictionary
+                    //print(convertedJsonIntoDict)
+                    let businesses = convertedJsonIntoDict["businesses"]! as! NSArray
+                    let sortedBusinesses: NSArray = businesses.sort({$0["rating"] as! Double == $1["rating"] as! Double ?
+                        $0["review_count"] as! Int > $1["review_count"] as! Int : $0["rating"] as! Double > $1["rating"] as! Double})
+                    print("\(sortedBusinesses)")
+                    
+                    //let indexOfFisrtUnqualifiedBusiness = sortedBusinesses.indexOfObjectPassingTest({ $0["rating"] < 4.5 }) // TODO: Why this not work?
+                    //print("indexOfFisrtUnqualifiedBusiness: \(indexOfFisrtUnqualifiedBusiness)")
+                    
+                    var index = 0
+                    for business in sortedBusinesses {
+                        let businessRating = business["rating"] as! Double
+                        if businessRating < 4 {
+                            index = sortedBusinesses.indexOfObject(business)
+                            //print("\(index)")
+                            break
+                        }
+                    }
+                    // Randomly pick one business from all qualified businesses(pick one from element < index).
+                    let randomNumber = Int(arc4random_uniform(UInt32(index)))
+                    print("random no. \(randomNumber), business: \(sortedBusinesses[randomNumber])")
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+}
--- randomRestaurant/ViewController.swift
+++ randomRestaurant/ViewController.swift
@@ -12,76 +12,30 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var token: UITextField!
     
+    private var brain = RestaurantBrain()
+    
     @IBAction func start() {
-        let myUrl = "https://api.yelp.com/v3/businesses/search?term=chinese&latitude=37.786882&longitude=-122.399972&limit=20"
-        
-        let urlObj = NSURL(string: myUrl)
-        
-        let request = NSMutableURLRequest(URL: urlObj!)
-        
-        request.HTTPMethod = "GET"
-        
+
+        // TODO: make params come from button/list
+        brain.getUrlParameters("chinese", latitude: 37.786882, longitude: -122.399972, limit: 20)
+        brain.makeBizSearchUrl("https://api.yelp.com/v3/businesses/search?")
+
         // Use this in production
         let access_token = token.text!
-        
 
         
-        request.addValue("Bearer \(access_token)", forHTTPHeaderField: "Authorization")
-        
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
-            data, response, error in
-            
-            // Check for error
-            if error != nil
-            {
-                print("error=\(error)")
-                return
-            }
-            
-            // Print out response string
-            //let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
-            //print("responseString = \(responseString)")
-            
-            // Convert server json response to NSDictionary
-            do {
-                if let convertedJsonIntoDict = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
-                    
-                    // Print out dictionary
-                    //print(convertedJsonIntoDict)
-                    let businesses = convertedJsonIntoDict["businesses"]! as! NSArray
-                    let sortedBusinesses: NSArray = businesses.sort({$0["rating"] as! Double == $1["rating"] as! Double ?
-                        $0["review_count"] as! Int > $1["review_count"] as! Int : $0["rating"] as! Double > $1["rating"] as! Double})
-                    print("\(sortedBusinesses)")
-                    
-                    //let indexOfFisrtUnqualifiedBusiness = sortedBusinesses.indexOfObjectPassingTest({ $0["rating"] < 4.5 }) // TODO: Why this not work?
-                    //print("indexOfFisrtUnqualifiedBusiness: \(indexOfFisrtUnqualifiedBusiness)")
-                    
-                    for business in sortedBusinesses {
-                        let businessRating = business["rating"] as! Double
-                        if businessRating < 4 {
-                            let index = sortedBusinesses.indexOfObject(business)
-                            print("\(index)")
-                            break
-                        }
-                    }
-                }
-            } catch let error as NSError {
-                print(error.localizedDescription)
-            }
-        }
-        task.resume()
+        brain.makeUrlRequest(access_token)
     }
     
+/*
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+*/
 }
 
