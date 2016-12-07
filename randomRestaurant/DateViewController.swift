589b7479aea3c5f27b53212a4ef57dd8ff331208
zuchie <zuchie@gmail.com>
2016-12-06 23:50:17 -0800

    Finished clock Hour & Minute arms rotation feature.

--- randomRestaurant/DateViewController.swift
+++ randomRestaurant/DateViewController.swift
@@ -13,14 +13,10 @@ class DateViewController: UIViewController {
     @IBOutlet weak var clockArmHour: UIImageView!
     @IBOutlet weak var clockArmHourWidthConstraint: NSLayoutConstraint!
     @IBOutlet weak var clockArmHourHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var clockArmHourXPositionConstraint: NSLayoutConstraint!
-    @IBOutlet weak var clockArmHourYPositionConstraint: NSLayoutConstraint!
 
     @IBOutlet weak var clockArmMinute: UIImageView!
     @IBOutlet weak var clockArmMinuteWidthConstraint: NSLayoutConstraint!
     @IBOutlet weak var clockArmMinuteHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var clockArmMinuteXPositionConstraint: NSLayoutConstraint!
-    @IBOutlet weak var clockArmMinuteYPositionConstraint: NSLayoutConstraint!
 
     @IBOutlet weak var clockDial: UIImageView!
     
@@ -40,41 +36,13 @@ class DateViewController: UIViewController {
         let touchPositionToCenterY = touchPosition.y - clockDial.bounds.size.height / 2
         // Get touch position angle.
         let touchPositionAngle = atan2(touchPositionToCenterY, touchPositionToCenterX) + hourArmToXAxisAngle
+        let clockMinuteArmAngle = getClockMinuteArmAngle(by: touchPositionAngle)
+        
+        // Rotate clock arms.
+        self.clockArmHour.transform = (self.clockArmHourAffineTransform?.rotated(by: touchPositionAngle))!
+        self.clockArmMinute.transform = (self.clockArmMinuteAffineTransform?.rotated(by: clockMinuteArmAngle))!
         
         /*
-        // Make rotation clockwise.
-        if touchPositionAngle < 0 {
-            touchPositionAngle += CGFloat(2 * M_PI)
-        }
-        */
-        
-        print("touch position: \(touchPositionAngle * CGFloat(180.0 / M_PI))")
-        
-        /*
-        var clockMinuteArmAngle = getClockMinuteArmAngle(by: touchPositionAngle)
-        
-        var HourArmRotationLegs = [CGFloat]()
-        var MinuteArmRotationLegs = [CGFloat]()
-        
-        let temp = touchPositionAngle
-        
-        while touchPositionAngle > CGFloat(M_PI) {
-            let rotationAngle: CGFloat = 179.0 * CGFloat(M_PI / 180.0)
-            touchPositionAngle -= rotationAngle
-            HourArmRotationLegs.append(rotationAngle)
-            print("hour rotation leg: \(rotationAngle * CGFloat(180.0 / M_PI))")
-        }
-        HourArmRotationLegs.append(temp)
-        
-        while clockMinuteArmAngle > CGFloat(M_PI) {
-            let rotationAngle: CGFloat = 179.0 * CGFloat(M_PI / 180.0)
-            clockMinuteArmAngle -= rotationAngle
-            MinuteArmRotationLegs.append(rotationAngle)
-        }
-        MinuteArmRotationLegs.append(touchPositionAngle)
-        */
-        
-        // Rotate clock arm.
         UIView.animate(
             withDuration: 1.0,
             delay: 0,
@@ -92,76 +60,11 @@ class DateViewController: UIViewController {
                     print("animation done")
                 }
         })
-
-    }
-    
-    @IBAction func handleClockArmRotation(_ sender: UITapGestureRecognizer) {
-        let touchPosition = sender.location(in: sender.view)
-        // Move coordinate system from upperleft corner to center.
-        let touchPositionToCenterX = touchPosition.x - sender.view!.bounds.size.width / 2
-        let touchPositionToCenterY = touchPosition.y - sender.view!.bounds.size.height / 2
-        // Get touch position angle.
-        var touchPositionAngle = atan2(touchPositionToCenterY, touchPositionToCenterX) + hourArmToXAxisAngle
-        
-        // Make rotation clockwise.
-        if touchPositionAngle < 0 {
-            touchPositionAngle += CGFloat(2 * M_PI)
-        }
-        
-        print("touch position: \(touchPositionAngle * CGFloat(180.0 / M_PI))")
-        
-        var clockMinuteArmAngle = getClockMinuteArmAngle(by: touchPositionAngle)
-
-        var HourArmRotationLegs = [CGFloat]()
-        var MinuteArmRotationLegs = [CGFloat]()
-        
-        let temp = touchPositionAngle
-        
-        while touchPositionAngle > CGFloat(M_PI) {
-            let rotationAngle: CGFloat = 179.0 * CGFloat(M_PI / 180.0)
-            touchPositionAngle -= rotationAngle
-            HourArmRotationLegs.append(rotationAngle)
-            print("hour rotation leg: \(rotationAngle * CGFloat(180.0 / M_PI))")
-        }
-        HourArmRotationLegs.append(temp)
-        
-        while clockMinuteArmAngle > CGFloat(M_PI) {
-            let rotationAngle: CGFloat = 179.0 * CGFloat(M_PI / 180.0)
-            clockMinuteArmAngle -= rotationAngle
-            MinuteArmRotationLegs.append(rotationAngle)
-        }
-        MinuteArmRotationLegs.append(touchPositionAngle)
-        
-        // Rotate clock arm.
-        UIView.animate(
-            withDuration: 1.0,
-            delay: 0,
-            options: [],
-            animations: {
-                for rotation in HourArmRotationLegs {
-                    print("rotation: \(rotation * CGFloat(180.0 / M_PI))")
-                    self.clockArmHour.transform = (self.clockArmHourAffineTransform?.rotated(by: rotation))!
-                }
-                /*
-                for rotation in MinuteArmRotationLegs {
-                    self.clockArmMinute.transform = (self.clockArmMinuteAffineTransform?.rotated(by: rotation))!
-                }
-                */
-            },
-            completion: { finished in
-                if finished {
-                    print("animation done")
-                }
-            })
-        
+        */
     }
 
     private func getClockMinuteArmAngle(by clockHourArmAngle: CGFloat) -> CGFloat {
-        //let minutesAngleByHourArm = clockHourArmAngle.truncatingRemainder(dividingBy: CGFloat(M_PI / 6))
         let minuteToHourAngularVelocity: CGFloat = 12.0
-        print("hour: \(clockHourArmAngle * CGFloat(180.0 / M_PI))")
-        //print("minute: \(minutesAngleByHourArm * minuteToHourAngularVelocity * CGFloat(180.0 / M_PI))")
-        //return minutesAngleByHourArm * minuteToHourAngularVelocity
         return clockHourArmAngle * minuteToHourAngularVelocity
     }
     
